8a22498455b5f16cbaff107aa276c0eb42cd5438
abillas <47216690+abillas@users.noreply.github.com>
2019-02-13 15:08:50 +0100

    Add Geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -10,21 +10,49 @@ socket.on("disconnect", function() {
 
 socket.on("newMessage", function(message) {
   console.log("newMessage", message);
-  var li = $("<li></li>");
+  var li = jQuery("<li></li>");
   li.text(`${message.from}: ${message.text}`);
 
-  $("#messages").append(li);
+  jQuery("#messages").append(li);
 });
 
-$("#message-form").on("submit", function(e) {
+socket.on("newLocationMessage", function(message) {
+  var li = jQuery("<li></li>");
+  var a = jQuery("<a target='_blank'>My Location</a>");
+
+  li.text(`${message.from}: `);
+  a.attr("href", message.url);
+  li.append(a);
+  jQuery("#messages").append(li);
+});
+
+jQuery("#message-form").on("submit", function(e) {
   e.preventDefault();
 
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: $("[name=message]").val()
+      text: jQuery("[name=message]").val()
     },
     function() {}
+  );
+});
+
+var locationButton = jQuery("#send-location");
+locationButton.on("click", function() {
+  if (!navigator.geolocation) {
+    return alert("NOt available Geolocation");
+  }
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      socket.emit("createLocationMessage", {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      });
+    },
+    function() {
+      alert("Unable to fetch location");
+    }
   );
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require("expect");
 
-var { generateMessage } = require("./message");
+var { generateMessage, generateLocationMessage } = require("./message");
 
 describe("generateMessage", () => {
   it("should generate correct message object", () => {
@@ -10,5 +10,18 @@ describe("generateMessage", () => {
 
     expect(typeof message.createdAt).toBe("number");
     expect(message).toMatchObject({ from, text });
+  });
+});
+
+describe("generateLocationMessage", () => {
+  it("should generate correct location", () => {
+    var from = "Me";
+    var lat = 15;
+    var lng = 20;
+    var url = "https://www.google.com/maps?q=15,20";
+    var message = generateLocationMessage(from, lat, lng);
+
+    expect(typeof message.createdAt).toBe("number");
+    expect(message).toMatchObject({ from, url });
   });
 });
